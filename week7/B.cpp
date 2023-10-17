87cf537dfa00bb758582d3cd7cc7777939993891
Hectorfr <hector.freard@gmail.com>
2023-10-17 20:29:55 +0200

    Change in format

--- week7/B.cpp
+++ week7/B.cpp
@@ -15,33 +15,5 @@ int main() {
         getline(cin, str);
         arr[i] = str;
     }
-
-    int pos = 0;
-    while(pos < n){
-        stringstream ss(arr[pos]);
-        ss >> str;
-
-        if (str == "inc"){
-            ss >> str;
-            m[str]++;
-            pos++;
-        }else if (str == "dec"){
-            ss >> str;
-            if (m[str] > 0 && m.count(str) != 0){
-                m[str]--;
-                pos++;
-                ss >> str; // pass the number
-            }else{
-                m[str] = 0;
-                ss >> str;
-                pos = stoi(str); // change to this instruction
-            }
-        }else{
-            break;
-        }
-    }
-    for (const auto& pair : m){
-        cout << pair.first << " = " << pair.second << endl;
-    }
     return 0;
 }
