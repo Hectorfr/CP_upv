026850f83015eeb2220d771df544bf1a7b71e733
Hectorfr <hector.freard@gmail.com>
2023-10-25 11:41:31 +0200

    C done

--- week8/C.cpp
+++ week8/C.cpp
@@ -13,8 +13,7 @@ int main() {
         if (i+a <= n) arr[i+a] = max(arr[i+a], arr[i]+1);
         if (i+b <= n) arr[i+b] = max(arr[i+b], arr[i]+1);
         if (i+c <= n) arr[i+c] = max(arr[i+c], arr[i]+1);
-    } 
-
+    }
     cout << arr[n] << endl;
     return 0;
 }
