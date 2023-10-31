5a5fd6afa5d0f8c070b32180097e3d2aada93bdf
Hectorfr <hector.freard@gmail.com>
2023-10-31 22:44:14 +0100

    Done first dp extra

--- week9/A.cpp
+++ week9/A.cpp
@@ -4,18 +4,25 @@ using namespace std;
 #define REPm(i, a, b) for (int i = a; i <= b; ++i)
 #define ent '\n'
 
-int main() {
-    int N;
-    long long a, b, c;
-    cin >> N >> a >> b >> c;
-    cout << a << " " << b << " ";
 
-    REP(i, 2, N){
-        cout << c << " ";
-        long long temp = c;
-        c = a + b + c;
-        a = b;
-        b = temp;
-    }
-    return 0;
+int main() {
+    int n, m; cin >> n >> m;
+    int dp[n][m];
+    REP(i, 0, n){
+        REP(j, 0, m){
+            cin >> dp[i][j];
+            if (i == 0 && j == 0) continue;
+            if (i != 0 && j != 0){ //can go left and up
+                dp[i][j] += max(dp[i-1][j], dp[i][j-1]); 
+            }else if (j == 0){ //can go up only
+                dp[i][j] += dp[i-1][j]; 
+            }else if (i == 0){ //can go left only
+                dp[i][j] += dp[i][j-1]; 
+            }
+        }
+    } 
+
+    cout << dp[n-1][m-1];
+    
+
 }
