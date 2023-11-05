6419ebe5a7b2081f967c5f9bfdcbefa2a8be0f10
Hectorfr <hector.freard@gmail.com>
2023-11-05 19:55:18 +0100

    D finished

--- week9/D.cpp
+++ week9/D.cpp
@@ -4,24 +4,23 @@ using namespace std;
 #define REPm(i, a, b) for (int i = a; i <= b; ++i)
 #define ent '\n'
 
+
 int main() {
     int n; cin >> n;
     int count = 0;
-    while(n != 1){
-        if (n % 2 == 0){
-            n /= 2;
-            count++;
-        }else if (n % 3 == 0){
-            n /= 3;
-            count++;
-        }else{
-            n--;
-            count++;
-        }
+    int arr[n+2];
+    memset(arr, 0, sizeof(arr));
+    arr[0] = 0; arr[1] = 1; arr[2] = 1;
+    int aux;
 
+    for(int i = 3, j = 4; i < n; i++, j++){
+        aux = arr[i-1];
+        if (j % 2 == 0) aux = min(aux, arr[j/2-1]);
+        if (j % 3 == 0) aux = min(aux, arr[j/3-1]);
+        arr[i] = aux +1 ;
     }
-    
-    cout << count;
+
+    cout << arr[n-1];
     
 
 }
