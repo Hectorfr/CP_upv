f65b1af39b17af2ca8d32282f5917685a123d44b
Hectorfr <hector.freard@gmail.com>
2023-11-11 20:33:22 +0100

    Week 9 finished

--- week9/F.cpp
+++ week9/F.cpp
@@ -3,26 +3,22 @@ using namespace std;
 #define REP(i, a, b) for (int i = a; i < b; ++i)
 #define REPm(i, a, b) for (int i = a; i <= b; ++i)
 #define ent '\n'
+#define MOD 1000000007
 
 
 int main() {
-    int n, m; cin >> n >> m;
-    int dp[n][m];
-    REP(i, 0, n){
-        REP(j, 0, m){
-            cin >> dp[i][j];
-            if (i == 0 && j == 0) continue;
-            if (i != 0 && j != 0){ //can go left and up
-                dp[i][j] += max(dp[i-1][j], dp[i][j-1]); 
-            }else if (j == 0){ //can go up only
-                dp[i][j] += dp[i-1][j]; 
-            }else if (i == 0){ //can go left only
-                dp[i][j] += dp[i][j-1]; 
-            }
+    int n, k; cin >> n >> k;
+
+    int dp[n];
+    dp[0] = 1;
+    REP(i, 1, n){
+        dp[i] = 0;
+        REP(j, 0, k+1){
+            if (i-j >= 0) dp[i] = (dp[i] + dp[i-j]) % MOD;
         }
     } 
 
-    cout << dp[n-1][m-1];
+    cout << dp[n-1];
     
 
 }
