0b5a625836855283244066d747de1f3c0cb18e41
Hectorfr <hector.freard@gmail.com>
2023-11-14 20:46:01 +0100

    Only two left, C done

--- week11/C.cpp
+++ week11/C.cpp
@@ -3,46 +3,21 @@ using namespace std;
 #define REP(i, a, b) for (int i = a; i < b; ++i)
 #define REPm(i, a, b) for (int i = a; i <= b; ++i)
 #define ent '\n'
+#define ll long long
 
 int main() {
-    int a, b, c; cin >> a >> b >> c;
+
+    int sides[3];
+    REP(i, 0, 3) cin >> sides[i];
+    sort(sides, sides+3);
+    int s = sides[0], m = sides[1], l = sides[2];
+
     
-    if (a == b && a == c){
-        cout << "EQUILATERO";
-        return 0;
-    }else if (a*a + b*b == c*c){
-        cout << "RECTANGULO";
-        return 0;
-    }else if (a == b || a == c || b == c){
-        cout << "ISOSCELES";
-        return 0;
-    }else if (a == b || a == c || b == c){
-        cout << "ISOSCELES";
-        return 0;
-    }else{
-        int sides[3];
-        sides[0] = a; sides[1] = b; sides[2] =c;
-
-        int maxN = 0;
-        maxN = max(a, b);
-        maxN = max(maxN, c);
-
-        int sum = 0;
-        REP(i, 0, 3){
-            if (sides[i] != maxN){
-                sum += sides[i];
-            }
-        }
-
-        if (sum > maxN){ //sum of both is bigger than the biggest
-            cout << "ESCALENO";
-            return 0;
-        }else{
-            cout << "IMPOSIBLE";
-            return 0;
-        }
-
-    }
+    if (s == l) cout << "EQUILATERO";
+    else if (s*s+m*m == l*l) cout << "RECTANGULO";
+    else if (s+m <= l) cout << "IMPOSIBLE";
+    else if (s == m || m == l) cout << "ISOSCELES";
+    else cout << "ESCALENO";
     
 
 }
